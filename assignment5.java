b5b1b40420686f1a42bec9302e151ac617c32d96
RoryStiff2003 <rorystiff2003@gmail.com>
2023-02-23 23:24:19 -0500

    charlie see

--- assignment5.java
+++ assignment5.java
@@ -10,15 +10,20 @@ public class assignment5 {
  * Description: This class recreates the process of validating credit cards
  */
 public class CreditCardValidation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         
         Scanner input = new Scanner(System.in);
 
-        	// Prompt the user to enter a credit card number as a long integer
-            System.out.println("Enter a credit card number as a long integer: "); 
+            //tell the user to enter a credit card number as long
+            System.out.println("Enter a credit card number as a long: ");
+            //assign variable as a long and use input.nextLong
             long creditCardNumber = input.nextLong();
-
-           int max;
+            
+   
+            
+            //call the methode validate validateCreditCardNumber 
+            validateCreditCard(creditCardNumber);
+   
         
                     
     }
@@ -63,8 +68,27 @@ public class CreditCardValidation {
      */
     public static int calculateEvenPositionSum(long creditCardNumber){
         
-        return 0;
+        int sum = 0; // create a variable to return the sum
+        String num = creditCardNumber + ""; //converte to a string
+        for (int i = getCreditCardSize(creditCardNumber) - 2; i >= 0; i -= 2){
+            sum += getDigit(Integer.parseInt(num.charAt(i) + "") * 2);//this method is used to parse the string value to an integer value
+            
+        }
+        return sum; //return sum to evenplace
 
+    }
+
+
+        //returns this number if it is a single digit, otherwise,
+    // return the sum of two digits
+    public static int getDigit(int creditCardNumber){
+        if (creditCardNumber < 9){ //if card number is less than 9 return the peremater creditcard number
+            return creditCardNumber;
+        }
+
+        else{
+            return creditCardNumber / 10 + creditCardNumber % 10; //else return credit card number modulus 10
+        }
     }
 
 
@@ -76,11 +100,20 @@ public class CreditCardValidation {
      * @return Returns the sum of the odd position numbers in the creditCardNumber parameter.
      */
     public static int calculateOddPositionSum(long creditCardNumber){
-        int sum = 0;
-        String num = creditCardNumber + "";
+        int sum = 0; // craete a sum var to return later
+        String num = creditCardNumber + ""; //convert to a string
+        for (int i = getCreditCardSize(creditCardNumber) - 1; i >= 0; i -= 2){
+            sum += Integer.parseInt(num.charAt(i) + ""); //parse the string to an integer value
+        }
+
+        return sum; //return the sum of odd place
+    }
         
     }
 
-}
+
+    
+
+
 
 
